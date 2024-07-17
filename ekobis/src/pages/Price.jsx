e525128a38afe1d70bcd50251726a333becb1c0e
Ali Can KANBOLAT <kanbolat.alican@gmail.com>
2024-07-17 14:02:23 +0300

    Delete ekobis directory

--- ekobis/src/pages/Price.jsx
+++
@@ -1,9 +0,0 @@
-
-
-export default function Price() {
-  return(
-    <div className="font-bold text-xl flex items-center justify-center flex-col">
-      <p>Urun FİYATLARI</p> 
-    </div>
-  )
-}
--- ekobis/src/pages/UrunListele.jsx
+++
@@ -1,9 +0,0 @@
-
-
-export default function UrunListele() {
-  return(
-    <div className="font-bold text-xl flex items-center justify-center flex-col">
-      <p>URUNLER LISTESI...</p> 
-    </div>
-  )
-}
